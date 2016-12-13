9bc52cb179457203d10ed7599e8d46f2ada74fba
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-12-13 22:31:05 +0100

    More anotations deleted

--- src/routes_user.php
+++ src/routes_user.php
@@ -94,41 +94,7 @@ $app->options(
 )->setName('miw_options_users');
 
 /**
- * Summary: Creates a new user
- * Notes: Creates a new user
- *
- * @SWG\Post(
- *     method      = "POST",
- *     path        = "/users",
- *     tags        = { "Users" },
- *     summary     = "Creates a new user",
- *     description = "Creates a new user",
- *     operationId = "miw_post_users",
- *     parameters  = {
- *          {
- *          "name":        "data",
- *          "in":          "body",
- *          "description": "`User` properties to add to the system",
- *          "required":    true,
- *          "schema":      { "$ref": "#/definitions/UserData" }
- *          }
- *     },
- *     @SWG\Response(
- *          response    = 201,
- *          description = "`Created` User created",
- *          schema      = { "$ref": "#/definitions/User" }
- *     ),
- *     @SWG\Response(
- *          response    = 400,
- *          description = "`Bad Request` Username or email already exists.",
- *          schema      = { "$ref": "#/definitions/Message" }
- *     ),
- *     @SWG\Response(
- *          response    = 422,
- *          description = "`Unprocessable entity` Username, e-mail or password is left out",
- *          schema      = { "$ref": "#/definitions/Message" }
- *     )
- * )
+ * @var \Slim\App $app
  */
 $app->post(
     '/users',
@@ -144,42 +110,7 @@ $app->post(
 )->setName('miw_post_users');
 
 /**
- * Summary: Updates a user
- * Notes: Updates the user identified by &#x60;userId&#x60;.
- *
- * @SWG\Put(
- *     method      = "PUT",
- *     path        = "/users/{userId}",
- *     tags        = { "Users" },
- *     summary     = "Updates a user",
- *     description = "Updates the user identified by `userId`.",
- *     operationId = "miw_put_users",
- *     parameters={
- *          { "$ref" = "#/parameters/userId" },
- *          {
- *          "name":        "data",
- *          "in":          "body",
- *          "description": "`User` data to update",
- *          "required":    true,
- *          "schema":      { "$ref": "#/definitions/UserData" }
- *          }
- *     },
- *     @SWG\Response(
- *          response    = 200,
- *          description = "`Ok` User previously existed and is now updated",
- *          schema      = { "$ref": "#/definitions/User" }
- *     ),
- *     @SWG\Response(
- *          response    = 400,
- *          description = "`Bad Request` User name or e-mail already exists",
- *          schema      = { "$ref": "#/definitions/Message" }
- *     ),
- *     @SWG\Response(
- *          response    = 404,
- *          description = "`Not Found` The user could not be found",
- *          schema      = { "$ref": "#/definitions/Message" }
- *     )
- * )
+ * @var \Slim\App $app
  */
 $app->put(
     '/users/{id:[0-9]+}',
