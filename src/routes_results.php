e27e0348f65c5e88a04c12d1a73d2a74b46db8f5
Jorge Higuera <jorge.higuera.fombuena@gmail.com>
2016-12-13 22:39:07 +0100

    Created basic api structure

--- src/routes_results.php
+++ src/routes_results.php
@@ -4,7 +4,9 @@
 
 use Swagger\Annotations as SWG;
 
-
+/**
+ * @var \Slim\App $app
+ */
 $app->get(
     '/results',
     function ($request, $response, $args) {
@@ -26,7 +28,9 @@ $app->get(
     }
 )->setName('miw_cget_results');
 
-
+/**
+ * @var \Slim\App $app
+ */
 $app->get(
     '/results/{id:[0-9]+}',
     function ($request, $response, $args) {
@@ -47,3 +51,81 @@ $app->get(
         return $response->withJson($result);
     }
 )->setName('miw_get_results');
+
+
+/**
+ * @var \Slim\App $app
+ */
+$app->delete(
+    '/results/{id:[0-9]+}',
+    function ($request, $response, $args) {
+        $this->logger->info('DELETE \'/results/' . $args['id'] . '\'');
+        $em = getEntityManager();
+        $result = $em
+            ->getRepository('MiW16\Results\Entity\Result')
+            ->findOneById($args['id']);
+        if (empty($result)) {  // 404 - User id. not found
+            $newResponse = $response->withStatus(404);
+            $datos = array(
+                'code' => 404,
+                'message' => 'User not found'
+            );
+            return $this->renderer->render($newResponse, 'message.phtml', $datos);
+        } else {
+            $em->remove($result);
+            $em->flush();
+        }
+
+        return $response->withStatus(204);
+    }
+)->setName('miw_delete_results');
+
+
+/**
+ * @var \Slim\App $app
+ */
+$app->options(
+    '/results',
+    function ($request, $response, $args) {
+        $this->logger->info('OPTIONS \'/results\'');
+
+        return $response
+            ->withHeader(
+                'Allow',
+                'OPTIONS, GET, POST, PUT, DELETE'
+            );
+    }
+)->setName('miw_options_results');
+
+/**
+ * @var \Slim\App $app
+ */
+$app->post(
+    '/results',
+    function ($request, $response, $args) {
+        $this->logger->info('POST \'/results\'');
+        $data = json_decode($request->getBody(), true); // parse the JSON into an assoc. array
+        // process $data...
+
+        // TODO
+        $newResponse = $response->withStatus(501);
+        return $newResponse;
+    }
+)->setName('miw_post_results');
+
+/**
+ * @var \Slim\App $app
+ */
+$app->put(
+    '/results/{id:[0-9]+}',
+    function ($request, $response, $args) {
+        $this->logger->info('PUT \'/results\'');
+        $data = json_decode($request->getBody(), true); // parse the JSON into an assoc. array
+        // process $data...
+
+        // TODO
+        $newResponse = $response->withStatus(501);
+        return $newResponse;
+    }
+)->setName('miw_post_results');
+
